f3fbae1b74243f1232c4c95ab7d1df819bcaf213
Danny Smooke <dana.smooke@gmail.com>
2021-02-05 18:30:45 -0500

    added updateOne function into burger const (for last three commits: referenced unit13-activity17-cat.js)

--- models/burger.js
+++ models/burger.js
@@ -1,17 +1,21 @@
+const connection = require("../config/connection.js");
 const ORM = require("../config/orm.js");
 // Import the ORM to create functions that will interact with the database
 
 // Write code that will call the ORM functions (selectAll, insertOne, updateOne)
 
 const burger = {
-    selectAll(callback) {
-        ORM.all('burger', (res) => callback(res));
-    },
+  selectAll(callback) {
+    ORM.all("burger", (res) => callback(res));
+  },
 
-    insertOne(cols, vals, callback) {
-        ORM.insertOne('burger', cols, vals, (res) => callback(res));
-    },
+  insertOne(cols, vals, callback) {
+    ORM.insertOne("burger", cols, vals, (res) => callback(res));
+  },
 
-    updateOne()
-}
+  updateOne(objColVals, condition, callback) {
+    ORM.updateOne("burger", objColVals, condition, (res) => callback(res));
+  },
+};
+
 module.exports = burger;
