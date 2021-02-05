e14d579238d4a67420a015a15b41403991e10b18
Danny Smooke <dana.smooke@gmail.com>
2021-02-05 18:27:19 -0500

    added selectAll function within burger const

--- models/burger.js
+++ models/burger.js
@@ -1,5 +1,15 @@
 const ORM = require("../config/orm.js");
+// Import the ORM to create functions that will interact with the database
 
 // Write code that will call the ORM functions (selectAll, insertOne, updateOne)
 
+const burger = {
+    selectAll(callback) {
+        ORM.all('burger', (res) => callback(res));
+    },
+
+    insertOne()
+
+    updateOne()
+}
 module.exports = burger;
