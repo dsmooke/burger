cc637e3692703b56ed71a883cf0bc92d91a88b7c
Danny Smooke <dana.smooke@gmail.com>
2021-02-05 18:46:13 -0500

    added put request for updateOne method by accessing id

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -15,12 +15,32 @@ router.get("/", (req, res) => {
   });
 });
 
-router.post("/api/burgers", (req, res) => {
+router.post("/api/burger", (req, res) => {
   burger.insertOne(
     ["burger_name", "devoured"],
     [req.body.burger_name, req.body.devoured],
     (result) => {
       res.json({ id: result.insertId });
+    }
+  );
+});
+
+router.put("/api/burger/:id", (req, res) => {
+  const condition = `id = ${req.params.id}`;
+
+  console.log("condition", condition);
+
+  burger.updateOne(
+    {
+      devoured: req.body.devoured,
+    },
+    condition,
+    (result) => {
+      if (result.changedRows === 0) {
+        // If no rows were changed, the the ID must not exist, so 404 error
+        return res.status(404).end();
+      }
+      res.status(200).end();
     }
   );
 });
