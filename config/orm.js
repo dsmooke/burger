1a88048057e5f946ff666ae6a4e7e56a1efb1fb7
Danny Smooke <dana.smooke@gmail.com>
2021-02-05 18:15:38 -0500

    added updateOne orm method (referenced unit13-activity17-orm.js)

--- config/orm.js
+++ config/orm.js
@@ -2,17 +2,17 @@ const connection = require("./connection");
 
 // Object for all our SQL statement functions, cb: callback argument
 const orm = {
-    selectAll(tableInput, callback) {
-        const queryString = `SELECT * FROM ${tableInput};`; 
-        connection.query(queryString, (err, result) => {
-            if (err) {
-                throw err;
-            }
-            callback(result);
-        });
-    },
+  selectAll(tableInput, callback) {
+    const queryString = `SELECT * FROM ${tableInput};`;
+    connection.query(queryString, (err, result) => {
+      if (err) {
+        throw err;
+      }
+      callback(result);
+    });
+  },
 
-insertOne(table, cols, vals, cb) {
+  insertOne(table, cols, vals, cb) {
     let queryString = `INSERT INTO ${table}`;
 
     queryString += " (";
@@ -21,19 +21,35 @@ insertOne(table, cols, vals, cb) {
     queryString += "VALUES (";
     queryString += printQuestionMarks(vals.length);
     queryString += ") ";
-    
+
     console.log(queryString);
 
     connection.query(queryString, vals, (err, result) => {
-        if (err) {
-            throw err;
-        }
+      if (err) {
+        throw err;
+      }
 
-        callback(result);
+      callback(result);
     });
-},
+  },
 
-updateOne();
+  updateOne(table, objColVals, condition, callback) {
+    let queryString = `UPDATE ${table}`;
+
+    queryString += " SET";
+    queryString += objToSql(objColVals);
+    queryString += " WHERE ";
+    queryString += condition;
+
+    console.log(queryString);
+    connection.query(queryString, (err, result) => {
+      if (err) {
+        throw err;
+      }
+      callback(result);
+    });
+  },
+};
 
 module.exports = ORM;
 
