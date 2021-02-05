56b728ca5ea220c1f3c7823b6a3f359efc3783b6
Danny Smooke <dana.smooke@gmail.com>
2021-02-05 15:15:42 -0500

    added instructional comments

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,7 +1,10 @@
 const express = require("express");
-const app = express();
 const router = express.Router();
 
+// Import the model (burger.js) to use its database functions
 const burger = require("../models/burger.js");
 
+// Create all our routes and set up logic within those routes where required.
+
+// Export routes for server.js to use
 module.exports = router;
