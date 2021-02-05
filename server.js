07963ba9c9fe755aae341eff785ab6a9391007ec
Danny Smooke <dana.smooke@gmail.com>
2021-02-05 15:21:28 -0500

    set handlebars - rearranged

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const exphbs = require("express-handlebars");
 
 // process.env.PORT lets the port be set by Heroku
 const PORT = process.env.PORT || 3360;
@@ -12,6 +11,9 @@ app.use(express.static("public"));
 // Parse request body as JSON
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+// Set Handlebars
+const exphbs = require("express-handlebars");
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
