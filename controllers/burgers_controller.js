fe03217c657d72f7e9d099489b2c2d92141e6098
Danny Smooke <dana.smooke@gmail.com>
2021-02-05 18:34:23 -0500

    added get request for selectAll method (added reference comment at bottom)

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -5,6 +5,17 @@ const router = express.Router();
 const burger = require("../models/burger.js");
 
 // Create all our routes and set up logic within those routes where required.
+router.get("/", (req, res) => {
+  burger.selectAll((data) => {
+    const burgerObj = {
+      burger: data,
+    };
+    console.log(burgerObj);
+    res.render("index", burgerObj);
+  });
+});
 
 // Export routes for server.js to use
 module.exports = router;
+
+//(referenced unit13-activity17-catsController.js)
