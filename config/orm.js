617487795bf1de0d3f00c1488b4ebccfd873c30f
Danny Smooke <dana.smooke@gmail.com>
2021-02-05 18:07:56 -0500

    added orm const with selectAll method (referenced unit13-activity17-orm.js)

--- config/orm.js
+++ config/orm.js
@@ -1,6 +1,16 @@
 const connection = require("./connection");
 
-selectAll();
+// Object for all our SQL statement functions, cb: callback argument
+const orm = {
+    selectAll(tableInput, callback) {
+        const queryString = `SELECT * FROM ${tableInput};`; 
+        connection.query(queryString, (err, result) => {
+            if (err) {
+                throw err;
+            }
+            callback(result);
+        });
+    },
 
 insertOne();
 
