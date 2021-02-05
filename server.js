3f3b11ac290f4bf28d2539ea1128c31ca45bc95c
Danny Smooke <dana.smooke@gmail.com>
2021-02-05 15:20:46 -0500

    added parse request body as JSON

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ const app = express();
 // Serve static content for the app from the "public" directory in the application directory
 app.use(express.static("public"));
 
+// Parse request body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
