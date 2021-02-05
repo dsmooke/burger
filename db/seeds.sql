fb453a64a85307da0e9af70a4ca19d073f3b525d
Danny Smooke <dana.smooke@gmail.com>
2021-02-05 15:14:15 -0500

    edited seeds.sql, removed id number

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,7 +1,7 @@
 USE burger_db;
 
-INSERT INTO burgers (id, burger_name, devoured)
-VALUES  (1, "Cheeseburger", FALSE),
-        (2, "Junior Whopper", FALSE),
-        (3, "Big Mac", FALSE);
+INSERT INTO burgers (burger_name, devoured)
+VALUES  ("Cheeseburger", FALSE),
+        ("Junior Whopper", FALSE),
+        ("Big Mac", FALSE);
 
